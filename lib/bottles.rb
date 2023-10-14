3651674b94cde1c11a3a8f5e603ee4f46d3ae1e1
Abdulrahman Qaddoumi <aoaq.1998@gmail.com>
2023-10-14 21:37:05 +0300

    Refactor the text builder readability

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -7,26 +7,20 @@ class Bottles
         total_bottles_label = get_bottle_label(bottles_count)
         remaining_bottles_label = get_bottle_label(@bottles_count)
 
-        if (bottles_count == 0)
-            return <<-string
-No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, 99 bottles of beer on the wall.
-    string
-        elsif (bottles_count == 1)
-            return <<-string
-#{bottles_count} #{total_bottles_label} of beer on the wall, #{bottles_count} #{total_bottles_label} of beer.
-Take it down and pass it around, no more bottles of beer on the wall.
-            string
-        else 
-        <<-string
-#{bottles_count} #{total_bottles_label} of beer on the wall, #{bottles_count} #{total_bottles_label} of beer.
-Take one down and pass it around, #{@bottles_count} #{remaining_bottles_label} of beer on the wall.
-        string
+        case bottles_count
+        when 0
+            "No more bottles of beer on the wall, no more bottles of beer.\n" \
+            "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+        when 1
+            "#{bottles_count} #{total_bottles_label} of beer on the wall, #{bottles_count} #{total_bottles_label} of beer.\n" \
+            "Take it down and pass it around, no more bottles of beer on the wall.\n"
+        else
+            "#{bottles_count} #{total_bottles_label} of beer on the wall, #{bottles_count} #{total_bottles_label} of beer.\n" \
+            "Take one down and pass it around, #{@bottles_count} #{remaining_bottles_label} of beer on the wall.\n"
         end
     end
 
     def verses(max_index, min_index)
-        
         verses = []
         max_index.downto(min_index) do |index|
             verses << verse(index)
